a00490a052244702f6cdc463f6cbd8f2a0b333ff
vgoer <goerblue@gmail.com>
2025-01-01 15:13:07 +0800

    add: openport 端口扫描脚本扫描

--- openport.sh
+++ openport.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #|---/ /+--------------------------+------/ /|#
 #|--/ /-|subdomain installation script|--/ /-|#
-#|-/ /--|        子域名脚本            |-/ /--|#
+#|-/ /--|        端口扫描脚本           |-/ /--|#
 #|/ /---+----------------------------+/ /---|#
 
 # 定义颜色变量
@@ -26,5 +26,26 @@ cat << "EOF"
 ---------------------------------------------------------
                                 v1.0.0 - @vgoer
 EOF
+echo -e "${NC}"
+
+# 输入主域名
+read -p "请输入主域名: " website_input
+
+# 创建输出目录
+if [ ! -d "$website_input" ]; then
+    if ! mkdir -p "$website_input"; then
+        echo "错误：无法创建输出目录 '$website_input'"
+        exit 1
+    fi
+fi
+
+
+echo -e "${BLUE}开始端口扫描...${NC}"
+echo -e "${YELLOW}[1/2] 使用naabu扫描...${NC}"
+naabu -host $website_input -c 50 -nmap-cli 'nmap -sV -sC' -o $website_input/$website_input-full.txt
+echo -e "${YELLOW}✓[2/2] naabu扫描完成${NC}"
+
+echo -e "${GREEN}端口扫描完成... 结果在 ${BLUE}$website_input/$website_input-full.txt${NC}"
+
 
 
